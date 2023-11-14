14f174e651ec243b52f48a7e8e3e701bb53c96ea
Booze33 <tebetisloh@gmail.com>
2023-11-14 12:56:21 +0100

    update api server

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :user_codes, only: [:index, :show, :create, :update, :destroy]
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    sessions: 'devise_token_auth/sessions'
-  }
-
-  delete '/auth/sign_out', to: 'application#logout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
